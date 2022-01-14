06ec203fecbe6bb2ac34ad08840b010e38033e93
Carlos Francisco Silva Junior <jradm87@gmail.com>
2022-01-13 23:59:30 -0300

    Flask project tutorial

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/cadastro')
+def cadastro():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('cadastro.html', title='cadastro', user=user, posts=posts)
