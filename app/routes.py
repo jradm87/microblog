cf83b859950ff41cb65e796c99dcd8e8c5580a5f
Carlos Francisco Silva Junior <jradm87@gmail.com>
2022-01-18 23:29:08 -0300

    Form virtual env

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,7 +16,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='System', user=user, posts=posts)
 
 @app.route('/cadastro')
 def cadastro():
@@ -31,3 +32,12 @@ def cadastro():
         }
     ]
     return render_template('cadastro.html', title='cadastro', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
